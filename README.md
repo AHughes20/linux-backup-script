e5ebc8226412fca67556e6be7544208bc4c7a1e8
Aaron H <caaronh85@gmail.com>
2026-05-04 02:05:04 -0400

    Task: add backup script and README

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-# RHEL based backup script
+# Linux Backup Script
 
-## Overview
-More to come
+Simple bash script that creates compressed backups of a specified directory.
+
+## Usage
+sudo ./backup.sh /path/to/dir
+
+## Features
+- Root privilege check
+- Input validation
+- Timestamped tar.gz backups
+- Safe path handling
---
+++ backup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Author: AHughes20
+# Date: 2026-05-04
+
+# Configuration
+# backup /etc, /home, and maybe other dirs soon
+
+#script should run auto 2/day and also when executed manually
+
+if [ "$EUID" -ne 0 ]; then
+    echo "run as root (sudo/su)"
+    exit 1    # exit code 1 is used for user error(commonly missing input)
+fi
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <directory>" 
+    exit 1
+fi
+if [ -d "$1" ]; then
+    dir=$(basename "$1")
+    mkdir -p /var/backups
+    tar -czvf "/var/backups/${dir}_$(date +%F).tar.gz" "$1"
+else
+    echo "Error: '$1' is not valid directory."   
+    exit 2     # exit code 2 is used for invalid input
+fi    
